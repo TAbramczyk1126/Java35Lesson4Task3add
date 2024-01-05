4b283750d33da15809d644fe32326fa91f941bf0
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-05 20:36:32 +0100

    final version, 2 array

--- Lesson5TaskAdd3.java
+++ Lesson5TaskAdd3.java
@@ -3,7 +3,7 @@ public class Lesson5TaskAdd3 {
     public static void main(String[] args) {
 
         System.out.println(getLargest(new int[]{88, 72, 22, 33}));
-        System.out.println(getLargest2(33, 22, 11, 44));
+        System.out.println(getLargest2(new int[]{33, 22, 11, 44}));
     }
 
     public static int getLargest(int[] numbers) {
